4fef305050c29768ba93f9d0c114c08996c005a5
Alvin Ericsson <alvin.eriksson@elev.ga.lbs.se>
2019-01-22 08:54:47 +0100

    Camera_Follow_Alvin

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+
+
+    private void FixedUpdate() //Gör så att den följer efter spelaren. Jag sätter då på detta skriptet på kameran så att den följer efter spelaren.
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+}
