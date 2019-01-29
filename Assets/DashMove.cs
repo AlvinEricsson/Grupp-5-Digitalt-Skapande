3d1e79208a7b444448e156737c9934826cb6faa7
david.pohlman <david.pohlman@elev.ga.lbs.se>
2019-01-29 10:51:16 +0100

    Dash Cooldown

--- Assets/DashMove.cs
+++ Assets/DashMove.cs
@@ -1,59 +1,89 @@
-﻿ using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DashMove : MonoBehaviour
 {
-    public KeyCode dashRight;
-    public KeyCode dashLeft;
+    //Dash Forward
+    public static KeyCode dashRight;
+    public static KeyCode dashLeft;
     private Rigidbody2D rbody;
     public float dashSpeed;
     private float dashTime;
     public float startDashTime;
     private int direction;
-    
+
+    //Cooldown
+    public bool isDashing = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Dash Forward
         rbody = GetComponent<Rigidbody2D>();
         dashTime = startDashTime;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (direction == 0)
+        if (isDashing == false)
         {
-            if(Input.GetKeyDown(dashRight) || Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.D))
+
+            if (Input.GetKeyDown(dashRight) || Input.GetKeyDown(KeyCode.D))
             {
                 direction = 1;
             }
-            else if(Input.GetKeyDown(dashLeft) || Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.A))
+            else if (Input.GetKeyDown(dashLeft) || Input.GetKeyDown(KeyCode.A))
             {
                 direction = 2;
+            }
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                isDashing = true;
             }
         }
         else
         {
-            if(dashTime <= 0)
+            if (dashTime <= 0)
             {
                 direction = 0;
                 dashTime = startDashTime;
                 rbody.velocity = Vector2.zero;
+                isDashing = false;
             }
             else
             {
                 dashTime -= Time.deltaTime;
 
-                if(direction == 1)
+                if (direction == 1)
                 {
                     rbody.velocity = Vector2.right * dashSpeed;
                 }
-                else if(direction == 2)
+                else if (direction == 2)
                 {
                     rbody.velocity = Vector2.left * dashSpeed;
                 }
             }
+
         }
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+  
+
