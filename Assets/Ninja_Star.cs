1eee69ea576d88be7fff3c2ef40c0b19aa4ffd0d
tobias <tobias.gustafsson2@elev.ga.lbs.se>
2019-02-05 10:22:40 +0100

    Hello world

---
+++ Assets/Ninja_Star.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ninja_Star : MonoBehaviour
+{
+    public float trowSpeed;
+    private Rigidbody2D rbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rbody.velocity = new Vector2(trowSpeed*transform.localScale.x, 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/TEstScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEstScript : MonoBehaviour
+{
+    public GameObject Trowing_Star;
+    public Transform trowPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            GameObject trowingStar =(GameObject)Instantiate(Trowing_Star, trowPoint.position, trowPoint.rotation);
+            trowingStar.transform.localScale = transform.localScale;
+        }
+    }
+}
