187533a0e30dd1b4e21d30cd9fcfc72d65d37bf3
gabriel.bozicstolt <gabriel.bozicstolt@elev.ga.lbs.se>
2019-02-05 10:40:41 +0100

    CameraBorder

---
+++ Assets/Scripts/CameraBorder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBorder : MonoBehaviour
+{
+    public bool border;
+
+    public float maxX;
+    public float minX;
+
+    public float maxY;
+    public float minY;
+
+    public float Z = -10;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(border==true)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX), Mathf.Clamp(transform.position.y, minY, maxY), transform.position.z);
+        }
+    }
+}
