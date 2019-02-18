53d3e201c2720d39e1778100d2f30fb0c18b5741
gabriel.bozicstolt <gabriel.bozicstolt@elev.ga.lbs.se>
2019-02-18 10:06:39 +0100

    Camera Border update
    + Player prefab
    player jump script changed (cleared up)

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -6,17 +6,14 @@ public class PlayerJump : MonoBehaviour
 {
     public float jumpHeight;
 
-    public bool doubleJump = false;
-
     public GroundCheck groundCheck;
     private Rigidbody2D rbody;
-    // Start is called before the first frame update
+
     void Start()
     {
        rbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
