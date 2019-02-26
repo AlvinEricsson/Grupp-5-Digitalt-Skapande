e0a5abfa8cbf39ea617f35b8d43454526f8d0434
Alvin Ericsson <alvin.eriksson@elev.ga.lbs.se>
2019-02-26 08:42:36 +0100

    ninja star add (works 2017) omg!

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    public float mSpeed;
+    
+    public float moveSpeed;
     Rigidbody2D rbody;
     public GroundCheck groundCheck;
     public float jumpSpeed;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
+        ;
     }
 
     // Update is called once per frame
@@ -21,7 +24,10 @@ public class movement : MonoBehaviour
 
         if (DashMove.disableMovement == true)
         {
-            rbody.velocity = new Vector2(Input.GetAxis("Horizontal") * mSpeed, rbody.velocity.y);
+            rbody.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rbody.velocity.y);
+
+
+
 
             if (Input.GetButtonDown("Jump"))
             {
@@ -30,6 +36,28 @@ public class movement : MonoBehaviour
                     rbody.velocity = new Vector2(rbody.velocity.x, jumpSpeed);
                 }
             }
+
+            if(Input.GetKeyDown(KeyCode.A))
+                {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+
+            if(Input.GetKeyDown(KeyCode.D))
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
         }
+
     }
+    
+
+        
+
+
+
+
+
+
+    
+
 }
