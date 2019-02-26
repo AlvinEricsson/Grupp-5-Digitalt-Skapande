60e01eaf1e531804b8b56e15e86ca660f3658124
faruk hadzigrahic <faruk.hadzigrahic@elev.ga.lbs.se>
2019-02-26 10:16:36 +0100

    Checkpoint_Player

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject currentCheckpoint;
+
+    private Movement player;
+
+    void Start()
+    {
+        player = FindObjectOfType<Movement>();
+    }
+
+    public void RespawnPlayer()
+    {
+        Debug.Log("Respawn PLayer");
+        player.transform.position = currentCheckpoint.transform.position;
+    }
+}
