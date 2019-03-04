8fbca7ec4a71e55038456d523f6bcd2a04c88a6b
gabriel.bozicstolt <gabriel.bozicstolt@elev.ga.lbs.se>
2019-03-04 09:56:57 +0100

    All functions added in movement script

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -7,28 +7,54 @@ public class movement : MonoBehaviour
     
     public float moveSpeed;
     Rigidbody2D rbody;
+    private Animator anim;
     public GroundCheck groundCheck;
     public float jumpSpeed;
+    private float dirX;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
-        ;
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        dirX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        if (Mathf.Abs(dirX) > 0 && rbody.velocity.y == 0)
+        { 
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+        }
+
+        if (rbody.velocity.y == 0)
+        {
+            anim.SetBool("IsJumping", false);
+            anim.SetBool("IsFalling", false);
+        }
+
+        if (rbody.velocity.y > 0)
+        {
+            anim.SetBool("IsJumping", true);
+        }
+
+        if (rbody.velocity.y < 0)
+        {
+            anim.SetBool("IsJumping", false);
+            anim.SetBool("IsFalling", true);
+        }
+
 
         if (DashMove.disableMovement == true)
         {
             rbody.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rbody.velocity.y);
-
-
-
-
             if (Input.GetButtonDown("Jump"))
             {
                 if (groundCheck.isGrounded > 0)
