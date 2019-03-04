a26784b6befe0b6903a6792d0ba312c8adc55db6
Alvin Ericsson <alvin.eriksson@elev.ga.lbs.se>
2019-03-04 12:55:44 +0100

    SAVE

---
+++ Assets/TurnBlueCheckpoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnBlueCheckpoint : MonoBehaviour
+{
+    private SpriteRenderer rend;
+    public Sprite Blue_flag;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+
+            rend.sprite = Blue_flag;
+    }
+}
