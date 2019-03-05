59591e94dd805aaf475f1ebe680b9c16d6f278cb
gabriel.bozicstolt <gabriel.bozicstolt@elev.ga.lbs.se>
2019-03-05 09:03:13 +0100

    Parallax effect and sorting
    Foreground and background parallax effect

--- Assets/ThrowNinjaStar.cs
+++ Assets/ThrowNinjaStar.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TEstScript : MonoBehaviour
+public class ThrowNinjaStar : MonoBehaviour
 {
     public float coolDown;
     public float coolDownTimer;
