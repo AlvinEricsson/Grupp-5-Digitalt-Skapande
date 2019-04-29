df9aa05792ee3146df203b4b5c4dda582ba4f732
Alvin Ericsson <alvin.eriksson@elev.ga.lbs.se>
2019-04-29 09:37:44 +0200

    birds working

--- Assets/Scripts/Follow_Player.cs
+++ Assets/Scripts/Follow_Player.cs
@@ -7,16 +7,28 @@ public class Follow_Player : MonoBehaviour
     private Transform target;
     public float speed;
     public bool followNow;
+
+    public bool facingRight = false;
+
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         followNow = false;
+
+
+
     }
 
     void Update()
     {
         FollowPlayer();
+        if (target.transform.position.x < gameObject.transform.position.x && facingRight)
+            Flip();
+        if (target.transform.position.x > gameObject.transform.position.x && !facingRight)
+            Flip();
+
+
     }
 
     public void FollowPlayer()
@@ -24,9 +36,16 @@ public class Follow_Player : MonoBehaviour
         if (followNow == true)
         {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            //transform.LookAt(target);
+
+
+            //transform.localScale *= -1f;
+
+
         }
-      
+
     }
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -34,5 +53,13 @@ public class Follow_Player : MonoBehaviour
         {
             followNow = true;
         }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 tmpScale = gameObject.transform.localScale;
+        tmpScale.x *= -1;
+        gameObject.transform.localScale = tmpScale;
     }
 }
