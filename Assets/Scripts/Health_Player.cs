8a891c37d1ab6af184918e1357cc467c2218e66f
Alvin Ericsson <alvin.eriksson@elev.ga.lbs.se>
2019-04-30 10:39:59 +0200

    fix and sorting

--- Assets/Scripts/Health_Player.cs
+++ Assets/Scripts/Health_Player.cs
@@ -103,9 +103,9 @@ public class Health_Player : MonoBehaviour
     {
         if (collision.gameObject.tag == "Hp_Pick_Up" && health < 5)
         {
+            Destroy(collision.gameObject);
             health = 1f + health;
             Debug.Log("Hello world");
-            Destroy(collision.gameObject);
             var sound = Instantiate(soundPickup, transform.position, Quaternion.identity);
             Destroy(sound, 1);
         }
