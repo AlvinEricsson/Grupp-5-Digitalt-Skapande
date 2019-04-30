feb53241a5bbeba876a0e48afd027c92202d9b06
faruk hadzigrahic <faruk.hadzigrahic@elev.ga.lbs.se>
2019-04-30 09:06:27 +0200

    spark commit

--- Assets/Scripts/Ninja_Star.cs
+++ Assets/Scripts/Ninja_Star.cs
@@ -6,37 +6,42 @@ public class Ninja_Star : MonoBehaviour
 {
     public float trowSpeed;
     private Rigidbody2D rbody;
+    public GameObject Star_Spark;
+
+    bool enableMovemnet = true;
     // Start is called before the first frame update
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         rbody.velocity = new Vector2(trowSpeed * transform.localScale.x, 0f);
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Enemy")
+        if (collision.tag != "Player")
         {
+            Instantiate(Star_Spark, transform.position, Star_Spark.transform.rotation);
+
             Destroy(gameObject);
         }
-        if (collision.gameObject.tag == "Wall")
-        {
-            Destroy(gameObject);
-        }
-        if (collision.gameObject.tag == "GroundEnemy")
-        {
-            Destroy(gameObject);
-        }
-        if (collision.gameObject.tag == "Ground")
-        {
-            Destroy(gameObject);
-        }
+
+
     }
 
+    private void CoolDownFunction()
+    {
+        if (ThrowNinjaStar.coolDownTimer >= 0)
+        {
+            ThrowNinjaStar.coolDownTimer -= Time.deltaTime;
+        }
+    }
 
 }
