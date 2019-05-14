8b7773cbe4151a44e09b894de8df7fe1081d11d7
gabriel.bozicstolt <gabriel.bozicstolt@elev.ga.lbs.se>
2019-05-14 08:16:20 +0200

    Up and Down attack animations

--- Assets/Scripts/AttackScript.cs
+++ Assets/Scripts/AttackScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AttackScript : MonoBehaviour
 {
     private bool attacking = false;
+    private bool upAttacking = false;
+    private bool downAttacking = false;
 
     private float attackTimer = 0;
     private float attackCD = 0.5f;
@@ -51,30 +53,33 @@ public class AttackScript : MonoBehaviour
                 attackTimer = attackCD;
 
                 attackTrigger.enabled = true;
+                
             }
         }
         if (direction == 2)
         {
             if (Input.GetKeyDown(KeyCode.E) && !attacking)
             {
-                attacking = true;
+                upAttacking = true;
                 attackTimer = attackCD;
 
                 attackTriggerUp.enabled = true;
+                
             }
         }
         if (direction == 3)
         {
             if (Input.GetKeyDown(KeyCode.E) && !attacking)
             {
-                attacking = true;
+                downAttacking = true;
                 attackTimer = attackCD;
 
                 attackTriggerDown.enabled = true;
+                
             }
         }
 
-        if (attacking)
+        if (attacking || upAttacking || downAttacking)
         {
             if (attackTimer > 0)
             {
@@ -83,11 +88,15 @@ public class AttackScript : MonoBehaviour
             else
             {
                 attacking = false;
+                downAttacking = false;
+                upAttacking = false;
                 attackTrigger.enabled = false;
                 attackTriggerUp.enabled = false;
                 attackTriggerDown.enabled = false;
             }
         }
         anim.SetBool("Attacking", attacking);
+        anim.SetBool("DownAttacking", downAttacking);
+        anim.SetBool("UpAttacking", upAttacking);
     }
 }
